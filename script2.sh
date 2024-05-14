1248b60d71fc80f5fbb6200c8854dadac029dd61
JavierWang61520 <118707992+JavierWang61520@users.noreply.github.com>
2024-05-14 15:12:16 +0800

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -7,5 +7,5 @@ pip install faiss-gpu
 cd ..
 mkdir yolo
 cd /content/AICUP_Baseline_BoT-SORT
-python fast_reid/datasets/generate_AICUP_patches.py --data_path <dataets_dir>/AI_CUP_MCMOT_dataset/train
-python yolov7/tools/AICUP_to_YOLOv7.py --AICUP_dir datasets/AI_CUP_MCMOT_dataset/train --YOLOv7_dir datasets/AI_CUP_MCMOT_dataset/yolo
+python fast_reid/datasets/generate_AICUP_patches.py --data_path /content/train
+python yolov7/tools/AICUP_to_YOLOv7.py --AICUP_dir /content/train --YOLOv7_dir /content/yolo
