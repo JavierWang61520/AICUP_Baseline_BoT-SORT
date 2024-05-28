7892b3389ea58c8fc57f5f9b9ed1745800dffaf3
JavierWang61520 <118707992+JavierWang61520@users.noreply.github.com>
2024-05-28 20:45:39 +0800

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,9 @@
+cd /content/AICUP_Baseline_BoT-SORT
+pip install numpy
+pip install -r requirements.txt
+pip install cython; pip3 install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+pip install cython_bbox
+pip install faiss-gpu
+cd ..
+mkdir yolo
+cd /content/AICUP_Baseline_BoT-SORT
