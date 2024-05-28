a562bb7fb2f843786b466ebdb5156cce66e9fd8d
JavierWang61520 <118707992+JavierWang61520@users.noreply.github.com>
2024-05-28 21:49:56 +0800

    Delete script0.sh

--- script0.sh
+++
@@ -1,9 +0,0 @@
-cd /content/AICUP_Baseline_BoT-SORT
-pip install numpy
-pip install -r requirements.txt
-pip install cython; pip3 install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
-pip install cython_bbox
-pip install faiss-gpu
-cd ..
-mkdir yolo
-cd /content/AICUP_Baseline_BoT-SORT
